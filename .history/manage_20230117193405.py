9223cc4a92c3bc609f45cfacf5505097ea386964
Kavin Jey <jeyakavin33@gmail.com>
2023-01-17 20:39:13 -0500

    adding new app content to house calls

--- .history/manage_20230117193405.py
+++ .history/manage_20230117193405.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lsquared.config")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lsqaured.config")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 
     try:
